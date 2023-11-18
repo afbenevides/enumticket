1c8bdc6d1a4b257ffc8e6982f416ecd499a5ab28
Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>
2023-11-17 23:25:27 -0500

    [ADD] config.py based configuration
    Signed-off-by: Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>

--- sample01.py
+++ sample01.py
@@ -1,49 +1,147 @@
 import ticket
 from reportlab.lib.units import mm
-from reportlab.lib.pagesizes import A4, LETTER
+from reportlab.lib.pagesizes import A3
 from reportlab.pdfgen import canvas
-from reportlab.lib.colors import red, black
+from reportlab.lib.colors import red
+import os
 
-t = ticket.Ticket(190 * mm, 65 * mm)
-t.add_drawable(ticket.Image("Ticket_FDAT_2023-2.png"))
+#choix si demo ou data privé
+
+# Replace this with your file path
+Data_file_path = 'data/config.py'
+
+# Check if the file exists
+if os.path.exists(Data_file_path):
+    # Code to execute if the file exists
+    print(f"File '{Data_file_path}' exists.")
+
+    from data import config
+
+    TICKETS_WIDTH_MM = config.TICKETS_WIDTH_MM
+    TICKETS_HEIGHT_MM = config.TICKETS_HEIGHT_MM
+
+    TICKETS_IMAGE_PATH = config.TICKETS_IMAGE_PATH
+
+    FIRST_COUNTER_X_POS_MM = config.FIRST_COUNTER_X_POS_MM
+    FIRST_COUNTER_Y_POS_MM = config.FIRST_COUNTER_Y_POS_MM
+
+    FIRST_COUNTER_BOX_WIDTH_MM = config.FIRST_COUNTER_BOX_WIDTH_MM
+    FIRST_COUNTER_BOX_LENGTH_MM = config.FIRST_COUNTER_BOX_LENGTH_MM
+    FIRST_COUNTER_BOX_BORDER_COLOR = config.FIRST_COUNTER_BOX_BORDER_COLOR
+    FIRST_COUNTER_BOX_BORDERWIDTH = config.FIRST_COUNTER_BOX_BORDERWIDTH
+
+    FIRST_COUNTER_POS_OFFSET_X = config.FIRST_COUNTER_POS_OFFSET_X
+    FIRST_COUNTER_POS_OFFSET_Y = config.FIRST_COUNTER_POS_OFFSET_Y
+    FIRST_COUNTER_FONTNAME = config.FIRST_COUNTER_FONTNAME
+    FIRST_COUNTER_FONTSIZE = config.FIRST_COUNTER_FONTSIZE
+
+    SECOND_COUNTER_X_POS_OFFSET_MM = config.SECOND_COUNTER_X_POS_OFFSET_MM
+    SECOND_COUNTER_Y_POS_OFFSET_MM = config.SECOND_COUNTER_Y_POS_OFFSET_MM
+
+    NB_TICKETS_TOTAL = config.NB_TICKETS_TOTAL
+    NB_TICKETS_PER_BUNDLE = config.NB_TICKETS_PER_BUNDLE
+
+    PAGE_LAYOUT_TICKETS_NUMOFFSET = config.PAGE_LAYOUT_TICKETS_NUMOFFSET
+    PAGE_LAYOUT_TICKETS_PAGESIZE = config.PAGE_LAYOUT_TICKETS_PAGESIZE
+    PAGE_LAYOUT_TICKETS_BLEED_MM = config.PAGE_LAYOUT_TICKETS_BLEED_MM
+    PAGE_LAYOUT_TICKETS_MARGIN_BOTTOM_MM = config.PAGE_LAYOUT_TICKETS_MARGIN_BOTTOM_MM
+    PAGE_LAYOUT_TICKETS_MARGIN_LEFT_MM = config.PAGE_LAYOUT_TICKETS_MARGIN_LEFT_MM
+    PAGE_LAYOUT_TICKETS_MARGIN_TOP_MM = config.PAGE_LAYOUT_TICKETS_MARGIN_TOP_MM
+    PAGE_LAYOUT_TICKETS_MARGIN_RIGHT_MM = config.PAGE_LAYOUT_TICKETS_MARGIN_RIGHT_MM
+
+    LAYOUT_GENERATION_ORDER = config.LAYOUT_GENERATION_ORDER
+    LAYOUT_GENERATION_CROPMARKS = config.LAYOUT_GENERATION_CROPMARKS
+    LAYOUT_GENERATION_INVERT = config.LAYOUT_GENERATION_INVERT
+
+
+
+    # Add more code here as needed
+else:
+    # Code to execute if the file does not exist
+    print(f"File '{Data_file_path}' does not exist.")
+    # Add more code here as needed
+    TICKETS_WIDTH_MM = 84
+    TICKETS_HEIGHT_MM = 62
+
+    TICKETS_IMAGE_PATH = "sample01.png"
+
+    FIRST_COUNTER_X_POS_MM = 17
+    FIRST_COUNTER_Y_POS_MM = 12
+
+    FIRST_COUNTER_BOX_WIDTH_MM = 30
+    FIRST_COUNTER_BOX_LENGTH_MM = 10
+    FIRST_COUNTER_BOX_BORDER_COLOR = red
+    FIRST_COUNTER_BOX_BORDERWIDTH = 1
+
+    FIRST_COUNTER_POS_OFFSET_X = 3
+    FIRST_COUNTER_POS_OFFSET_Y = 3
+    FIRST_COUNTER_FONTNAME = "Courier-Bold"
+    FIRST_COUNTER_FONTSIZE = 20
+
+    SECOND_COUNTER_X_POS_OFFSET_MM = 45
+    SECOND_COUNTER_Y_POS_OFFSET_MM = 0
+
+    NB_TICKETS_TOTAL = 100
+    NB_TICKETS_PER_BUNDLE = 5
+
+    PAGE_LAYOUT_TICKETS_NUMOFFSET = 100
+    PAGE_LAYOUT_TICKETS_PAGESIZE = A3
+    PAGE_LAYOUT_TICKETS_BLEED_MM = 2
+    PAGE_LAYOUT_TICKETS_MARGIN_BOTTOM_MM = 0
+    PAGE_LAYOUT_TICKETS_MARGIN_LEFT_MM = 0
+    PAGE_LAYOUT_TICKETS_MARGIN_TOP_MM = 0
+    PAGE_LAYOUT_TICKETS_MARGIN_RIGHT_MM = 0
+
+    LAYOUT_GENERATION_ORDER = ticket.STACKORDER
+    LAYOUT_GENERATION_CROPMARKS = True
+    LAYOUT_GENERATION_INVERT = True
+
+t = ticket.Ticket(TICKETS_WIDTH_MM * mm, TICKETS_HEIGHT_MM * mm)
+t.add_drawable(ticket.Image(TICKETS_IMAGE_PATH))
 # t.add_drawable(ticket.Border(color=red))
 
-# counter
-pos_x = 122 * mm
-pos_y = 4 * mm
+# 1rst counter position
+pos_x = FIRST_COUNTER_X_POS_MM * mm
+pos_y = FIRST_COUNTER_Y_POS_MM * mm
 
-t.add_drawable(ticket.Box(pos_x, pos_y, 19 * mm, 7 * mm, bordercolor=black, borderwidth=0.25 * mm))
-t.add_drawable(ticket.Counter(pos_x + 3 * mm, pos_y + 2.2 * mm, fontname='Courier-Bold', fontsize=12))
+t.add_drawable(ticket.Box(pos_x, pos_y, FIRST_COUNTER_BOX_WIDTH_MM * mm, FIRST_COUNTER_BOX_LENGTH_MM * mm, bordercolor=FIRST_COUNTER_BOX_BORDER_COLOR, borderwidth=FIRST_COUNTER_BOX_BORDERWIDTH * mm))
+t.add_drawable(ticket.Counter(pos_x + FIRST_COUNTER_POS_OFFSET_X * mm, pos_y + FIRST_COUNTER_POS_OFFSET_Y * mm, fontname=FIRST_COUNTER_FONTNAME, fontsize=FIRST_COUNTER_FONTSIZE))
 
-t.add_drawable(ticket.Box(pos_x + 45 * mm, pos_y, 19 * mm, 7 * mm, bordercolor=black, borderwidth=0.25 * mm))
-t.add_drawable(ticket.Counter(pos_x + 45 * mm + 3 * mm, pos_y + 2.2 * mm, fontname='Courier-Bold', fontsize=12))
+# Deactivate/activate second counter for raffle ticket double numerotation with False/True
+if True:
+    t.add_drawable(ticket.Box(pos_x + SECOND_COUNTER_X_POS_OFFSET_MM * mm, pos_y + SECOND_COUNTER_Y_POS_OFFSET_MM * mm, FIRST_COUNTER_BOX_WIDTH_MM * mm, FIRST_COUNTER_BOX_LENGTH_MM * mm, bordercolor=FIRST_COUNTER_BOX_BORDER_COLOR, borderwidth=FIRST_COUNTER_BOX_BORDERWIDTH * mm))
+    t.add_drawable(ticket.Counter(pos_x + SECOND_COUNTER_X_POS_OFFSET_MM * mm + FIRST_COUNTER_POS_OFFSET_X * mm, pos_y + SECOND_COUNTER_Y_POS_OFFSET_MM * mm + FIRST_COUNTER_POS_OFFSET_Y * mm, fontname=FIRST_COUNTER_FONTNAME, fontsize=FIRST_COUNTER_FONTSIZE))
 
 # Note: Available width = page width - 2* cropmark size - margin left - margin right (donc 210 - 5*2 , si 0 left et 0 right
 # LETTER size = 8.5 x 11 = 215.9mm   x 279.4
 # A4 size                = 210mm       297 mm
 
-Nb_billets_total = 3000
+# #enter here total number of tickets
+# NB_TICKETS_TOTAL = NB_TICKETS_TOTAL
 
-layout = ticket.PageLayout(t, Nb_billets_total, numoffset=1, pagesize=LETTER, bleed=0 * mm, bottom=0 * mm, left=5 * mm,
-                           top=1 * mm, right=5 * mm)
+#Creation of PDF with all tickets
+layout = ticket.PageLayout(t, NB_TICKETS_TOTAL, numoffset=PAGE_LAYOUT_TICKETS_NUMOFFSET, pagesize=PAGE_LAYOUT_TICKETS_PAGESIZE, bleed=PAGE_LAYOUT_TICKETS_BLEED_MM * mm, bottom=PAGE_LAYOUT_TICKETS_MARGIN_BOTTOM_MM * mm, left=PAGE_LAYOUT_TICKETS_MARGIN_LEFT_MM * mm,
+                           top=PAGE_LAYOUT_TICKETS_MARGIN_TOP_MM * mm, right=PAGE_LAYOUT_TICKETS_MARGIN_RIGHT_MM * mm)
 
-c = canvas.Canvas('FICHIER_complet.pdf', layout.pagesize)
+c = canvas.Canvas('data/Layout_0_complete_file.pdf', layout.pagesize)
 # print(c.getAvailableFonts())
 
-layout.generate(c, order=ticket.STACKORDER, cropmarks=True, invert=True)
+layout.generate(c, order=LAYOUT_GENERATION_ORDER, cropmarks=LAYOUT_GENERATION_CROPMARKS, invert=LAYOUT_GENERATION_INVERT)
 c.save()
 
-# # creation des print par paquet brochables
-# nb_billets_par_paquet = 5
-# nb_billets_par_page = layout.colcount * layout.rowcount
-# nb_billet_par_layout = (nb_billets_par_page * nb_billets_par_paquet)
-# nb_layout_a_produire = Nb_billets_total // nb_billet_par_layout
-#
-# for n in range(nb_layout_a_produire):
-#     layout = ticket.PageLayout(t, nb_billet_par_layout, numoffset=((n*nb_billet_par_layout)+1), pagesize=LETTER, bleed=2 * mm, bottom=0 * mm,
-#                                left=5 * mm, top=1 * mm, right=5 * mm)
-#
-#     c = canvas.Canvas('Layout_'+str(n)+'.pdf', layout.pagesize)
-#     # print(c.getAvailableFonts())
-#     layout.generate(c, order=ticket.STACKORDER, cropmarks=True, invert=True)
-#     c.save()
+# Creation of stapled bundles ready for prints
+# deactivate/activate bundle creation with False/True
+if True:
+    nb_ticket_per_stapled_bundle = NB_TICKETS_PER_BUNDLE
+    nb_tickets_per_page = layout.colcount * layout.rowcount
+    nb_tickets_per_layout = (nb_tickets_per_page * nb_ticket_per_stapled_bundle)
+    nb_layout_to_produce = NB_TICKETS_TOTAL // nb_tickets_per_layout
+
+    for n in range(nb_layout_to_produce):
+        layout = ticket.PageLayout(t, nb_tickets_per_layout, numoffset=((n * nb_tickets_per_layout) + PAGE_LAYOUT_TICKETS_NUMOFFSET), pagesize=PAGE_LAYOUT_TICKETS_PAGESIZE, bleed=PAGE_LAYOUT_TICKETS_BLEED_MM * mm, bottom=PAGE_LAYOUT_TICKETS_MARGIN_BOTTOM_MM * mm,
+                                   left=PAGE_LAYOUT_TICKETS_MARGIN_LEFT_MM * mm, top=PAGE_LAYOUT_TICKETS_MARGIN_TOP_MM * mm, right=PAGE_LAYOUT_TICKETS_MARGIN_RIGHT_MM * mm)
+
+        c = canvas.Canvas('data/Layout_'+str(n)+'.pdf', layout.pagesize)
+        # print(c.getAvailableFonts())
+        layout.generate(c, order=LAYOUT_GENERATION_ORDER, cropmarks=LAYOUT_GENERATION_CROPMARKS, invert=LAYOUT_GENERATION_INVERT)
+        c.save()
